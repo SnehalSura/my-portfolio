ed0cfa91619eefb2250316494a14943a6fc250f9
SnehalSura <snehalsura@gmail.com>
2025-04-21 20:52:50 +0530

    Added about section

--- portfolio_frontend/portfolio-react/src/Components/About/About.jsx
+++ portfolio_frontend/portfolio-react/src/Components/About/About.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import './About.css'
+import profile_img from '../../assets/profile_photo.jpg'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='about'>
+        <div className="about-title">
+            <h1>About me</h1>
+        </div>
+        <div className="about-section">
+            <div className="about-left">
+                <img src={profile_img} alt="profile image" height="420px" />
+            </div>
+            <div className="about-right">
+                <div className="about-para">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit dolores laboriosam laborum.</p>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta maxime, quasi labore totam voluptatibus facere!</p>
+                </div>
+                <div className="about-skills">
+                    <div className="about-skill"><p>HTML & CSS</p><hr style={{width: "60%"}} /></div>
+                    <div className="about-skill"><p>ReactJs</p><hr style={{width: "65%"}} /></div>
+                    <div className="about-skill"><p>JavaScript</p><hr style={{width: "70%"}} /></div>
+                    <div className="about-skill"><p>Django</p><hr style={{width: "70%"}} /></div>
+                </div>
+            </div>
+            {/* <div className="about-achievements"></div> */}
+        </div>
+    </div>
   )
 }
 
